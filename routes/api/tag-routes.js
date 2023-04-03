552e21e89cb4e5181504a3c775c5aa8d8b1920c8
Wes Harrison <wesharrison89@gmail.com>
2023-04-02 20:25:49 -0700

    updated sections of routes/api folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,42 +3,61 @@ const { Tag, Category, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
- Product.findAll({
-  include: [
-    {
-      model: Category,
-      attributes: ['id', 'category_name'],
-    },
-    {
-      model: Tag,
-      through: ProductTag,
-      as: 'tags'
-    },
-  ],
- })
- .then((products) => res.json(products))
- .catch((err) => {
-   console.log(err);
-   res.status(500).json(err)
- });
+router.get('/', async (req, res) => {
+  try {
+    const dataTag = await Tag.findAll({
+      include: [{model: Product, through: ProductTag, as: 'products'}],
+    });
+    res.status(200).json(dataTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const dataTag = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag, as: 'products'}],
+    });
+    res.status(200).json(dataTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
